646787603706c866bc2cc8d8408c492d397dbbbe
Fabian Czappa <czappa@cs.tu-darmstadt.de>
2020-12-10 14:23:08 +0100

    Adjust NeuronMonitor to secondary

--- relearn/source/NeuronMonitor.h
+++ relearn/source/NeuronMonitor.h
@@ -18,7 +18,7 @@ struct NeuronInformation {
 	double calcium;
 	double x;
 	bool fired;
-	bool refrac;
+	double secondary;
 	double I_sync;
 
 	double axons;
@@ -29,9 +29,9 @@ struct NeuronInformation {
 	double dendrites_inh_connected;
 
 	explicit NeuronInformation(
-		double c = 0.0, double x = 0.0, bool f = false, bool r = false, double i = 0.0,
+		double c = 0.0, double x = 0.0, bool f = false, double s = 0.0, double i = 0.0,
 		double ax = 0.0, double ax_c = 0.0, double de = 0.0, double de_c = 0.0, double di = 0.0, double di_c = 0.0) :
-		calcium(c), x(x), fired(f), refrac(r), I_sync(i), axons(ax), axons_connected(ax_c),
+		calcium(c), x(x), fired(f), secondary(s), I_sync(i), axons(ax), axons_connected(ax_c),
 		dendrites_exc(de), dendrites_exc_connected(de_c), dendrites_inh(di), dendrites_inh_connected(di_c) {
 	}
 };
@@ -62,7 +62,7 @@ public:
 		const double& calcium = neurons_to_monitor.calcium[target_neuron_id];
 		const double& x = neurons_to_monitor.neuron_models->x[target_neuron_id];
 		const bool& fired = neurons_to_monitor.neuron_models->fired[target_neuron_id];
-		const bool& refrac = neurons_to_monitor.neuron_models->get_secondary_variable(target_neuron_id) > 0;
+		const double& secondary = neurons_to_monitor.neuron_models->get_secondary_variable(target_neuron_id);
 		const double& I_sync = neurons_to_monitor.neuron_models->I_syn[target_neuron_id];
 
 		const double& axons = neurons_to_monitor.axons.cnts[target_neuron_id];
@@ -72,7 +72,7 @@ public:
 		const double& dendrites_inh = neurons_to_monitor.dendrites_inh.cnts[target_neuron_id];
 		const double& dendrites_inh_connected = neurons_to_monitor.dendrites_inh.connected_cnts[target_neuron_id];
 
-		informations[current_step] = NeuronInformation(calcium, x, fired, refrac, I_sync, axons, axons_connected, dendrites_exc, dendrites_exc_connected, dendrites_inh, dendrites_inh_connected);
+		informations[current_step] = NeuronInformation(calcium, x, fired, secondary, I_sync, axons, axons_connected, dendrites_exc, dendrites_exc_connected, dendrites_inh, dendrites_inh_connected);
 	}
 
 	const std::vector<NeuronInformation>& get_informations() const /*noexcept*/ {
