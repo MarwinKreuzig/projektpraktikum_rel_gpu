564e170ed7b1f928447c6dbcf0dd8478098651d7
Fabian Czappa <czappa@cs.tu-darmstadt.de>
2021-02-03 18:27:42 +0100

    Fix test issue

--- relearn/test/commons.cpp
+++ relearn/test/commons.cpp
@@ -1,5 +1,6 @@
 #include "commons.h"
 
+#include "../source/LogFiles.h"
 #include "../source/MPIWrapper.h"
 #include "../source/RelearnException.h"
 
@@ -13,6 +14,7 @@ void setup() {
     auto lambda = []() {
         char* argument = (char*)"./runTests";
         MPIWrapper::init(1, &argument);
+        LogFiles::init();
     };
 
     std::call_once(some_flag, lambda);
