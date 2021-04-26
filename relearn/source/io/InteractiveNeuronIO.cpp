5521a59135843a7f0c4db2fbc3726101535073e6
Fabian Czappa <fabian.czappa@tu-darmstadt.de>
2021-04-26 18:53:35 +0200

    Add functionality to load enable and disable commands

---
+++ relearn/source/io/InteractiveNeuronIO.cpp
@@ -0,0 +1,111 @@
+/*
+ * This file is part of the RELeARN software developed at Technical University Darmstadt
+ *
+ * Copyright (c) 2020, Technical University of Darmstadt, Germany
+ *
+ * This software may be modified and distributed under the terms of a BSD-style license.
+ * See the LICENSE file in the base directory for details.
+ *
+ */
+
+#include "InteractiveNeuronIO.h"
+
+#include "../util/RelearnException.h"
+
+#include <fstream>
+#include <iostream>
+#include <sstream>
+
+std::vector<std::pair<size_t, std::vector<size_t>>> InteractiveNeuronIO::load_enable_interrups(const std::string& path_to_file) {
+    std::ifstream file{ path_to_file };
+
+    const bool file_is_good = file.good();
+    const bool file_is_not_good = file.fail() || file.eof();
+
+    RelearnException::check(file_is_good && !file_is_not_good, "Opening the file was not successful");
+
+    std::vector<std::pair<size_t, std::vector<size_t>>> return_value;
+
+    for (std::string line{}; std::getline(file, line);) {
+        // Skip line with comments
+        if (!line.empty() && '#' == line[0]) {
+            continue;
+        }
+
+        std::stringstream sstream(line);
+
+        size_t step{};
+        char delim{};
+
+        bool success = (sstream >> step) && (sstream >> delim);
+
+        if (!success) {
+            std::cerr << "Skipping line: \"" << line << "\"\n";
+            continue;
+        }
+
+        if (delim != 'e') {
+            if (delim != 'd') {
+                std::cerr << "Wrong deliminator: \"" << line << "\"\n";
+            }
+            continue;
+        }
+
+        std::vector<size_t> indices;
+
+        for (size_t id{}; sstream >> id;) {
+            indices.emplace_back(id);
+        }
+
+        return_value.emplace_back(step, std::move(indices));
+    }
+
+    return return_value;
+}
+
+std::vector<std::pair<size_t, std::vector<size_t>>> InteractiveNeuronIO::load_disable_interrups(const std::string& path_to_file) {
+    std::ifstream file{ path_to_file };
+
+    const bool file_is_good = file.good();
+    const bool file_is_not_good = file.fail() || file.eof();
+
+    RelearnException::check(file_is_good && !file_is_not_good, "Opening the file was not successful");
+
+    std::vector<std::pair<size_t, std::vector<size_t>>> return_value;
+
+    for (std::string line{}; std::getline(file, line);) {
+        // Skip line with comments
+        if (!line.empty() && '#' == line[0]) {
+            continue;
+        }
+
+        std::stringstream sstream(line);
+
+        size_t step{};
+        char delim{};
+
+        bool success = (sstream >> step) && (sstream >> delim);
+
+        if (!success) {
+            std::cerr << "Skipping line: \"" << line << "\"\n";
+            continue;
+        }
+
+        if (delim != 'd') {
+            if (delim != 'e') {
+                std::cerr << "Wrong deliminator: \"" << line << "\"\n";
+            }
+            continue;
+        }
+
+        std::vector<size_t> indices;
+
+        for (size_t id{}; sstream >> id;) {
+            indices.emplace_back(id);
+        }
+
+        return_value.emplace_back(step, std::move(indices));
+    }
+
+    return return_value;
+}
---
+++ relearn/source/io/InteractiveNeuronIO.h
@@ -0,0 +1,24 @@
+/*
+ * This file is part of the RELeARN software developed at Technical University Darmstadt
+ *
+ * Copyright (c) 2020, Technical University of Darmstadt, Germany
+ *
+ * This software may be modified and distributed under the terms of a BSD-style license.
+ * See the LICENSE file in the base directory for details.
+ *
+ */
+
+#pragma once
+
+#include "../util/RelearnException.h"
+
+#include <string>
+#include <utility>
+#include <vector>
+
+class InteractiveNeuronIO {
+public:
+    static std::vector<std::pair<size_t, std::vector<size_t>>> load_enable_interrups(const std::string& path_to_file);
+
+    static std::vector<std::pair<size_t, std::vector<size_t>>> load_disable_interrups(const std::string& path_to_file);
+};
