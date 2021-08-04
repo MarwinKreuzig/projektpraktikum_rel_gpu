f7c3a018a30234d664b3599f7a97d79e128f47d5
Julian Schmidt <schmidt.julian99@gmail.com>
2021-08-04 17:33:21 +0200

    reuse V instead of calculating the number of vertices

--- graph/source/apsp/johnson.cpp
+++ graph/source/apsp/johnson.cpp
@@ -107,7 +107,7 @@ void johnson_parallel_impl(graph_t& gr, std::vector<double>& output) {
 #pragma omp parallel for schedule(dynamic)
 #endif
     for (int s = 0; s < V; s++) {
-        std::vector<double> d(boost::num_vertices(G));
+        std::vector<double> d(V);
         boost::dijkstra_shortest_paths(G, s, boost::distance_map(d.data()));
         for (int v = 0; v < V; v++) {
             output[static_cast<size_t>(s) * static_cast<size_t>(V) + static_cast<size_t>(v)] = d[v] + h[v] - h[s];
