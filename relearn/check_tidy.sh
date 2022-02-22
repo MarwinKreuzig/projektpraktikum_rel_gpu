c167bffa57acb9bb8592e0570b6da940b51aff32
Fabian Czappa <fabian.czappa@tu-darmstadt.de>
2022-02-22 09:21:58 +0100

    Add more ignored clang-tidy checks

--- relearn/check_tidy.sh
+++ relearn/check_tidy.sh
@@ -3,6 +3,6 @@
 for f in $(find ../source/ -name '*.cpp'); 
 do 
 echo "Processing ${f}"; 
-clang-tidy $f --checks=*,-clang-analyzer-core.CallAndMessage,-clang-analyzer-core.NonNullParamChecker,-openmp-exception-escape,-llvmlibc-restrict-system-libc-headers,-llvmlibc-callee-namespace,-llvmlibc-implementation-in-namespace,-modernize-use-trailing-return-type,-llvm-header-guard,-fuchsia-default-arguments-calls
+clang-tidy $f --checks=*,-clang-analyzer-core.CallAndMessage,-clang-analyzer-core.NonNullParamChecker,-openmp-exception-escape,-llvmlibc-restrict-system-libc-headers,-llvmlibc-callee-namespace,-llvmlibc-implementation-in-namespace,-modernize-use-trailing-return-type,-llvm-header-guard,-fuchsia-default-arguments-calls,-fuchsia-default-arguments-declarations,-cppcoreguidelines-avoid-non-const-global-variables,-altera-struct-pack-align,-fuchsia-overloaded-operator
 done
 
