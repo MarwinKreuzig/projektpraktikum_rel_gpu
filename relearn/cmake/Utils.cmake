cd5b7e16515b3d7f97a00e0be7f65960585f4987
Julian Schmidt <schmidt.julian99@gmail.com>
2022-09-29 17:12:21 +0200

    add check-coverage target
    - runs check then generates the html report
    - added sub targets to run collection and reporting as seperate steps

---
+++ relearn/cmake/Utils.cmake
@@ -0,0 +1,34 @@
+find_program(LCOV lcov)
+set(COVERAGE_REPORT_AVAILABLE OFF)
+if(NOT
+   LCOV
+   MATCHES
+   "-NOTFOUND")
+  add_custom_target(
+    coverage-collect
+    COMMAND
+      ${LCOV} --directory ${CMAKE_BINARY_DIR} --capture --output-file
+      ${CMAKE_BINARY_DIR}/coverage.info --exclude "*_deps*" --exclude
+      "/usr/include/*"
+    VERBATIM
+    WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
+    BYPRODUCTS ${CMAKE_BINARY_DIR}/coverage.info)
+
+  find_program(GENHTML genhtml)
+  if(NOT
+     GENHTML
+     MATCHES
+     "-NOTFOUND")
+    add_custom_target(
+      coverage-report
+      COMMAND ${GENHTML} --demangle-cpp -o coverage
+              ${CMAKE_BINARY_DIR}/coverage.info
+      DEPENDS coverage-collect
+      WORKING_DIRECTORY ${CMAKE_BINARY_DIR})
+    set(COVERAGE_REPORT_AVAILABLE ON)
+  elseif()
+    message("genhtml executable not found (required for coverage-report)")
+  endif()
+elseif()
+  message("lcov executable not found (required for coverage-collect)")
+endif()
