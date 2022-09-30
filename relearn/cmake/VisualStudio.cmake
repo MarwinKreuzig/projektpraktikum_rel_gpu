3b97a4563cd39c093f460a38256bf7c421163630
Fabian Czappa <fabian.czappa@tu-darmstadt.de>
2022-09-30 15:51:09 +0200

    Fix build stuff

--- relearn/cmake/VisualStudio.cmake
+++ relearn/cmake/VisualStudio.cmake
@@ -1,81 +1,127 @@
-file(GLOB_RECURSE SRC_ALG "${CMAKE_SOURCE_DIR}/source/algorithm/*")
-file(GLOB_RECURSE SRC_ALG_NAIVE
-     "${CMAKE_SOURCE_DIR}/source/algorithm/NaiveInternal/*")
-file(GLOB_RECURSE SRC_ALG_BH
-     "${CMAKE_SOURCE_DIR}/source/algorithm/BarnesHutInternal/*")
-file(GLOB_RECURSE SRC_ALG_FMM
-     "${CMAKE_SOURCE_DIR}/source/algorithm/FMMInternal/*")
-file(GLOB_RECURSE SRC_ALG_KERNEL
-     "${CMAKE_SOURCE_DIR}/source/algorithm/Kernel/*")
-file(GLOB_RECURSE SRC_IO "${CMAKE_SOURCE_DIR}/source/io/*")
-file(GLOB_RECURSE SRC_MPI "${CMAKE_SOURCE_DIR}/source/mpi/*")
-file(GLOB_RECURSE SRC_SIM "${CMAKE_SOURCE_DIR}/source/sim/*")
-file(GLOB_RECURSE SRC_SIM_FILE "${CMAKE_SOURCE_DIR}/source/sim/file/*")
-file(GLOB_RECURSE SRC_SIM_RANDOM "${CMAKE_SOURCE_DIR}/source/sim/random/*")
-file(GLOB_RECURSE SRC_STR "${CMAKE_SOURCE_DIR}/source/structure/*")
-file(GLOB_RECURSE SRC_UTIL "${CMAKE_SOURCE_DIR}/source/util/*")
-file(
-  GLOB_RECURSE
-  SRC_NEU
-  "${CMAKE_SOURCE_DIR}/source/neurons/*.h"
-  "${CMAKE_SOURCE_DIR}/source/neurons/*.cpp")
-file(GLOB_RECURSE SRC_NEU_MOD "${CMAKE_SOURCE_DIR}/source/neurons/models/*")
-file(GLOB_RECURSE SRC_NEU_HEL "${CMAKE_SOURCE_DIR}/source/neurons/helper/*")
-
-file(GLOB_RECURSE SRC_MAIN "${CMAKE_SOURCE_DIR}/source/main.cpp")
-file(GLOB_RECURSE SRC_RELEARN "${CMAKE_SOURCE_DIR}/source/main_relearn.cpp")
-file(GLOB_RECURSE SRC_ANALYSIS "${CMAKE_SOURCE_DIR}/source/main_analysis.cpp")
-file(GLOB_RECURSE SRC_CONF "${CMAKE_SOURCE_DIR}/source/Config.h")
-file(GLOB_RECURSE SRC_TYPES "${CMAKE_SOURCE_DIR}/source/Types.h")
-
-file(GLOB_RECURSE SRC_TST "${CMAKE_SOURCE_DIR}/test/*")
-file(GLOB_RECURSE SRC_BENCH " ${CMAKE_SOURCE_DIR}/benchmark/*")
-
-message("The path is:")
-message(${CMAKE_SOURCE_DIR})
-message(${SRC_UTIL})
-
-source_group(
-  TREE ${CMAKE_SOURCE_DIR}
-  FILES ${SRC_ALG}
-        ${SRC_ALG_NAIVE}
-        ${SRC_ALG_BH}
-        ${SRC_ALG_FMM}
-        ${SRC_ALG_KERNEL}
-        ${SRC_IO}
-        ${SRC_MPI}
-        ${SRC_SIM}
-        ${SRC_STR}
-        ${SRC_UTIL}
-        ${SRC_NEU}
-        ${SRC_NEU_MOD}
-        ${SRC_NEU_HEL}
-        ${SRC_MAIN}
-        ${SRC_CONF}
-        ${SRC_TYPES}
-        ${SRC_SIM_FILE}
-        ${SRC_SIM_RANDOM})
-
-source_group("tests\\" FILES ${SRC_TST})
-source_group("benchmark\\" FILES ${SRC_BENCH})
-
-add_library(
-  relearn_lib_2 STATIC
-  ${SRC_ALG}
-  ${SRC_ALG_NAIVE}
-  ${SRC_ALG_BH}
-  ${SRC_ALG_FMM}
-  ${SRC_ALG_KERNEL}
-  ${SRC_IO}
-  ${SRC_MPI}
-  ${SRC_SIM}
-  ${SRC_STR}
-  ${SRC_UTIL}
-  ${SRC_NEU}
-  ${SRC_NEU_MOD}
-  ${SRC_NEU_HEL}
-  ${SRC_CONF}
-  ${SRC_TYPES}
-  ${SRC_SIM_FILE}
-  ${SRC_SIM_RANDOM})
-
+set(relearn_lib_additional_files "" CACHE INTERNAL "")
+	
+if(WIN32)
+	# root files
+	list(APPEND relearn_lib_additional_files "Config.h")
+	list(APPEND relearn_lib_additional_files "Types.h")
+	
+    # algorithm
+	list(APPEND relearn_lib_additional_files "algorithm/Algorithm.h")
+	list(APPEND relearn_lib_additional_files "algorithm/AlgorithmEnum.h")
+	list(APPEND relearn_lib_additional_files "algorithm/Algorithms.h")
+	list(APPEND relearn_lib_additional_files "algorithm/Cells.h")
+	list(APPEND relearn_lib_additional_files "algorithm/Connector.h")
+	list(APPEND relearn_lib_additional_files "algorithm/VirtualPlasticityElement.h")
+	
+	# BarnesHutInternal
+	list(APPEND relearn_lib_additional_files "algorithm/BarnesHutInternal/BarnesHut.h")
+	list(APPEND relearn_lib_additional_files "algorithm/BarnesHutInternal/BarnesHutBase.h")
+	list(APPEND relearn_lib_additional_files "algorithm/BarnesHutInternal/BarnesHutCell.h")
+	list(APPEND relearn_lib_additional_files "algorithm/BarnesHutInternal/BarnesHutInverted.h")
+	list(APPEND relearn_lib_additional_files "algorithm/BarnesHutInternal/BarnesHutInvertedCell.h")
+	list(APPEND relearn_lib_additional_files "algorithm/BarnesHutInternal/BarnesHutLocationAware.h")
+	
+	# FMMInternal
+	list(APPEND relearn_lib_additional_files "algorithm/FMMInternal/FastMultipoleMethods.h")
+	list(APPEND relearn_lib_additional_files "algorithm/FMMInternal/FastMultipoleMethodsCell.h")
+	
+	# Internal
+	list(APPEND relearn_lib_additional_files "algorithm/Internal/AlgorithmImpl.h")
+	list(APPEND relearn_lib_additional_files "algorithm/Internal/ExchangingAlgorithm.h")
+	
+	# Kernel
+	list(APPEND relearn_lib_additional_files "algorithm/Kernel/Gamma.h")
+	list(APPEND relearn_lib_additional_files "algorithm/Kernel/Gaussian.h")
+	list(APPEND relearn_lib_additional_files "algorithm/Kernel/Kernel.h")
+	list(APPEND relearn_lib_additional_files "algorithm/Kernel/Linear.h")
+	list(APPEND relearn_lib_additional_files "algorithm/Kernel/Weibull.h")
+	
+	# Naive
+	list(APPEND relearn_lib_additional_files "algorithm/NaiveInternal/Naive.h")
+	list(APPEND relearn_lib_additional_files "algorithm/NaiveInternal/NaiveCell.h")
+	
+	# io
+	list(APPEND relearn_lib_additional_files "io/CalciumIO.h")
+	list(APPEND relearn_lib_additional_files "io/InteractiveNeuronIO.h")
+	list(APPEND relearn_lib_additional_files "io/LogFiles.h")
+	list(APPEND relearn_lib_additional_files "io/NeuronIO.h")
+	
+	# mpi
+	list(APPEND relearn_lib_additional_files "mpi/CommunicationMap.h")
+	list(APPEND relearn_lib_additional_files "mpi/MPINoWrapper.h")
+	list(APPEND relearn_lib_additional_files "mpi/MPIWrapper.h")
+	
+	# neurons
+	list(APPEND relearn_lib_additional_files "neurons/CalciumCalculator.h")
+	list(APPEND relearn_lib_additional_files "neurons/ElementType.h")
+	list(APPEND relearn_lib_additional_files "neurons/FiredStatus.h")
+	list(APPEND relearn_lib_additional_files "neurons/NetworkGraph.h")
+	list(APPEND relearn_lib_additional_files "neurons/Neurons.h")
+	list(APPEND relearn_lib_additional_files "neurons/NeuronsExtraInfo.h")
+	list(APPEND relearn_lib_additional_files "neurons/SignalType.h")
+	list(APPEND relearn_lib_additional_files "neurons/TargetCalciumDecay.h")
+	list(APPEND relearn_lib_additional_files "neurons/UpdateStatus.h")
+	
+	# neurons
+	list(APPEND relearn_lib_additional_files "neurons/helper/DistantNeuronRequests.h")
+	list(APPEND relearn_lib_additional_files "neurons/helper/NeuronMonitor.h")
+	list(APPEND relearn_lib_additional_files "neurons/helper/RankNeuronId.h")
+	list(APPEND relearn_lib_additional_files "neurons/helper/Synapse.h")
+	list(APPEND relearn_lib_additional_files "neurons/helper/SynapseCreationRequests.h")
+	list(APPEND relearn_lib_additional_files "neurons/helper/SynapseDeletionRequests.h")
+	
+	# neurons
+	list(APPEND relearn_lib_additional_files "neurons/models/BackgroundActivityCalculator.h")
+	list(APPEND relearn_lib_additional_files "neurons/models/BackgroundActivityCalculators.h")
+	list(APPEND relearn_lib_additional_files "neurons/models/FiredStatusCommunicationMap.h")
+	list(APPEND relearn_lib_additional_files "neurons/models/FiredStatusCommunicator.h")
+	list(APPEND relearn_lib_additional_files "neurons/models/ModelParameter.h")
+	list(APPEND relearn_lib_additional_files "neurons/models/NeuronModels.h")
+	list(APPEND relearn_lib_additional_files "neurons/models/SynapticElements.h")
+	list(APPEND relearn_lib_additional_files "neurons/models/SynapticInputCalculator.h")
+	list(APPEND relearn_lib_additional_files "neurons/models/SynapticInputCalculators.h")
+	
+	# sim
+	list(APPEND relearn_lib_additional_files "sim/LoadedNeuron.h")
+	list(APPEND relearn_lib_additional_files "sim/NeuronToSubdomainAssignment.h")
+	list(APPEND relearn_lib_additional_files "sim/Simulation.h")
+	list(APPEND relearn_lib_additional_files "sim/SynapseLoader.h")
+	
+	# file
+	list(APPEND relearn_lib_additional_files "sim/file/FileSynapseLoader.h")
+	list(APPEND relearn_lib_additional_files "sim/file/MultipleFilesSynapseLoader.h")
+	list(APPEND relearn_lib_additional_files "sim/file/MultipleSubdomainsFromFile.h")
+	list(APPEND relearn_lib_additional_files "sim/file/SubdomainFromFile.h")
+	
+	# random
+	list(APPEND relearn_lib_additional_files "sim/random/BoxBasedRandomSubdomainAssignment.h")
+	list(APPEND relearn_lib_additional_files "sim/random/RandomSynapseLoader.h")
+	list(APPEND relearn_lib_additional_files "sim/random/SubdomainFromNeuronDensity.h")
+	list(APPEND relearn_lib_additional_files "sim/random/SubdomainFromNeuronPerRank.h")
+	
+	# structure
+	list(APPEND relearn_lib_additional_files "structure/BaseCell.h")
+	list(APPEND relearn_lib_additional_files "structure/Cell.h")
+	list(APPEND relearn_lib_additional_files "structure/NodeCache.h")
+	list(APPEND relearn_lib_additional_files "structure/Octree.h")
+	list(APPEND relearn_lib_additional_files "structure/OctreeNode.h")
+	list(APPEND relearn_lib_additional_files "structure/Partition.h")
+	list(APPEND relearn_lib_additional_files "structure/SpaceFillingCurve.h")
+	
+	# util
+	list(APPEND relearn_lib_additional_files "util/MemoryHolder.h")
+	list(APPEND relearn_lib_additional_files "util/MonitorParser.h")
+	list(APPEND relearn_lib_additional_files "util/Random.h")
+	list(APPEND relearn_lib_additional_files "util/RelearnException.h")
+	list(APPEND relearn_lib_additional_files "util/SemiStableVector.h")
+	list(APPEND relearn_lib_additional_files "util/Stack.h")
+	list(APPEND relearn_lib_additional_files "util/StatisticalMeasures.h")
+	list(APPEND relearn_lib_additional_files "util/StepParser.h")
+	list(APPEND relearn_lib_additional_files "util/TaggedID.h")
+	list(APPEND relearn_lib_additional_files "util/Timers.h")
+	list(APPEND relearn_lib_additional_files "util/Utility.h")
+	list(APPEND relearn_lib_additional_files "util/Vec3.h")
+	
+	# shuffle
+	list(APPEND relearn_lib_additional_files "util/shuffle/shuffle.h")
+endif()
