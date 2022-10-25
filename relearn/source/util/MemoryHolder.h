4ed41ac67cafe884030300924366a2b532fbba19
Fabian Czappa <fabian.czappa@tu-darmstadt.de>
2022-10-25 14:47:35 +0200

    Fix typename

--- relearn/source/util/MemoryHolder.h
+++ relearn/source/util/MemoryHolder.h
@@ -69,7 +69,8 @@ public:
      * @brief Destroys all objects that were handed out via get_available. All pointers are invalidated.
      */
     static void make_all_available() noexcept {
-        for (std::span<OctreeNode<AdditionalCellAttributes>>::size_type i = 0; i < memory_holder.size(); i++) {
+        using size_type = typename std::span<OctreeNode<AdditionalCellAttributes>>::size_type;
+        for (size_type i = 0; i < memory_holder.size(); i++) {
             memory_holder[i].reset();
         }
 
