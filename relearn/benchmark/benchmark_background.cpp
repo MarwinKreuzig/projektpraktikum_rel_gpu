3b576cec074d3cb166094289250b515e2769f15b
Fabian Czappa <fabian.czappa@tu-darmstadt.de>
2022-12-22 12:49:30 +0100

    Add benckmark_background to cmake

--- relearn/benchmark/benchmark_background.cpp
+++ relearn/benchmark/benchmark_background.cpp
@@ -2,7 +2,7 @@
 
 #include "factory/background_factory.h"
 
-#include "neurons/UpdateStatus.h"
+#include "neurons/enums/UpdateStatus.h"
 
 static void BM_null_background_activity_0(benchmark::State& state) {
     const auto number_neurons = state.range(0);
