4476f8df42b1076d122c67361133c658bbcb625e
marvin <marvin.kaster@tu-darmstadt.de>
2023-03-21 15:04:59 +0100

    Fixed: MPI check

--- relearn/source/mpi/MPIWrapper.h
+++ relearn/source/mpi/MPIWrapper.h
@@ -509,8 +509,7 @@ public:
     */
     template<typename T>
     static void create_rma_window(MPIWindow::Window window_type, std::uint64_t number_elements, size_t number_ranks) {
-
-        RelearnException::check(!MPIWindow::mpi_windows[window_type]->initialized, "MPIWrapper::create_rma_window: Window {} is already created", window_type);
+        RelearnException::check(MPIWindow::mpi_windows[window_type] == nullptr, "MPIWrapper::create_rma_window: Window {} is already created", window_type);
 
         void* ptr = nullptr;
 
