f4edf189f906ab0182c99ed40d2e6f19c724a29d
Marwin Kreuzig <marwin@kreuzig.info>
2024-01-11 19:21:40 +0100

    fixed mistakes in last merge

--- relearn/source/structure/Octree.h
+++ relearn/source/structure/Octree.h
@@ -423,6 +423,7 @@ public:
         std::stack<std::pair<const OctreeNode<AdditionalCellAttributes> *, size_t>> octree_nodes{};
         octree_nodes.emplace(&root, 0);
 
+	// first, we find the number of nodes per level in the tree
         while (!octree_nodes.empty()) {
             const auto [current_node, level] = octree_nodes.top();
             octree_nodes.pop();
@@ -430,10 +431,10 @@ public:
             if (current_node->get_cell().get_neuron_id().is_virtual()) {
                 num_virtual_neurons++;
 
-                while (level_indices.size() <= level + 1) {
-                    level_indices.push_back(level_indices.back());
+                while (level_indices.size() <= level) {
+                    level_indices.push_back(0);
                 }
-                level_indices[level + 1] += 1;
+                level_indices[level] += 1;
 
                 const auto &children = current_node->get_children();
                 int child_count = 0;
@@ -447,7 +448,24 @@ public:
             }
         }
 
+	// then, we use the number of nodes er level to set the indices in level_indices
+	size_t nodes_above = 0;
+	for (auto & level_index : level_indices) {
+	    auto nodes_this_level = level_index;
+	    level_index = nodes_above;
+	    nodes_above += nodes_this_level;
+	}
 
+	octree_cpu_copy.minimum_cell_position_virtual.resize(num_virtual_neurons);
+	octree_cpu_copy.maximum_cell_position_virtual.resize(num_virtual_neurons);
+	octree_cpu_copy.position_excitatory_element_virtual.resize(num_virtual_neurons);
+	octree_cpu_copy.position_inhibitory_element_virtual.resize(num_virtual_neurons);
+	octree_cpu_copy.num_free_elements_excitatory_virtual.resize(num_virtual_neurons);
+	octree_cpu_copy.num_free_elements_inhibitory_virtual.resize(num_virtual_neurons);
+	octree_cpu_copy.num_children.resize(num_virtual_neurons);
+	for (auto & child_indices : octree_cpu_copy.child_indices) {
+	    child_indices.resize(num_virtual_neurons);
+	}
 
         std::stack<std::tuple<const OctreeNode<AdditionalCellAttributes> *, size_t, size_t>> octree_nodes_second_pass{};
         octree_nodes_second_pass.emplace(&root, 0, 0);
@@ -464,7 +482,13 @@ public:
                 const auto index = level_indices[level];
                 level_indices[level] += 1;
                 if (parent_index != 0) {
-                    octree_cpu_copy.child_indices[parent_index].push_back(index);
+		    // look for an unassigned space in the children array
+		    for (auto & child_indices : octree_cpu_copy.child_indices) {
+	                if (child_indices[parent_index] == 0) {
+			    child_indices[parent_index] = index;
+			    break;
+			}
+		    }
                 }
 		// Copy the neuron
                 // Currently assumes that either dendrites are both true or axons are both true
