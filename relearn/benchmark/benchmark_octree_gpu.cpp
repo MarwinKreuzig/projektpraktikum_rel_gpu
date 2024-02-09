7f84711312df73cbd8307d72ea9d113fbdbb8279
Julian Ewald <Julianaxel.ewald@gmail.com>
2024-02-09 15:27:28 +0100

    Created base for octree benchmark

---
+++ relearn/benchmark/benchmark_octree_gpu.cpp
@@ -0,0 +1,46 @@
+/*
+* This file is part of the RELeARN software developed at Technical University Darmstadt
+ *
+ * Copyright (c) 2020, Technical University of Darmstadt, Germany
+ *
+ * This software may be modified and distributed under the terms of a BSD-style license.
+ * See the LICENSE file in the base directory for details.
+ *
+ */
+
+#if CUDA_FOUND
+
+#include "main.h"
+#include "gpu/Octree.cuh"
+
+static void BM_octree_copy_100(benchmark::State& state) {
+
+}
+
+static void BM_octree_copy_1000(benchmark::State& state) {
+
+}
+
+static void BM_octree_copy_10000(benchmark::State& state) {
+
+}
+
+static void BM_octree_copy_100000(benchmark::State& state) {
+
+}
+
+static void BM_octree_copy_1000000(benchmark::State& state) {
+
+}
+
+static void BM_octree_copy_10000000(benchmark::State& state) {
+
+}
+
+static void BM_octree_copy_100000000(benchmark::State& state) {
+
+}
+
+static void BM_octree_copy_1000000000(benchmark::State& state) {
+
+}
