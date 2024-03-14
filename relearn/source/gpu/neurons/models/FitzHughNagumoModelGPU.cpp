eb530aef6dcf7005dc20fee5e0dd6cde906aa7ac
Marwin Kreuzig <marwin@kreuzig.info>
2024-03-14 20:05:31 +0100

    finished initial version

--- relearn/source/gpu/neurons/models/FitzHughNagumoModelGPU.cpp
+++ relearn/source/gpu/neurons/models/FitzHughNagumoModelGPU.cpp
@@ -3,7 +3,9 @@
 
 namespace gpu::models {
 FitzHughNagumoModelGPU::FitzHughNagumoModelGPU(const unsigned int h, std::unique_ptr<SynapticInputCalculator>&& synaptic_input_calculator,
-    std::unique_ptr<BackgroundActivityCalculator>&& background_activity_calculator, std::unique_ptr<Stimulus>&& stimulus_calculator, double a, double b, double phi) { }
+    std::unique_ptr<BackgroundActivityCalculator>&& background_activity_calculator, std::unique_ptr<Stimulus>&& stimulus_calculator, double _a, double _b, double _phi)
+    : NeuronModelGPU(create_poisson_model_data(), h, std::move(synaptic_input_calculator), std::move(background_activity_calculator), std::move(stimulus_calculator)), a {_a}, b{_b}, phi{_phi} {
+    }
 
 void FitzHughNagumoModelGPU::update_activity(RelearnGPUTypes::step_type step) {
     init_fitzhughnagumo_kernel(step, gpu_handle.get(), number_neurons, model_data_handle.get(), a, b, phi, init_w, init_x, scale, h);
--- relearn/source/gpu/neurons/models/IzhikevichModelGPU.cpp
+++ relearn/source/gpu/neurons/models/IzhikevichModelGPU.cpp
@@ -3,7 +3,8 @@
 
 namespace gpu::models {
 IzhikevichModelGPU::IzhikevichModelGPU(const unsigned int h, std::unique_ptr<SynapticInputCalculator>&& synaptic_input_calculator,
-    std::unique_ptr<BackgroundActivityCalculator>&& background_activity_calculator, std::unique_ptr<Stimulus>&& stimulus_calculator, double a, double b, double c, double d, double V_spike, double k1, double k2, double k3) { }
+    std::unique_ptr<BackgroundActivityCalculator>&& background_activity_calculator, std::unique_ptr<Stimulus>&& stimulus_calculator, double _a, double _b, double _c, double _d, double _V_spike, double _k1, double _k2, double _k3)
+    : NeuronModelGPU(create_poisson_model_data(), h, std::move(synaptic_input_calculator), std::move(background_activity_calculator), std::move(stimulus_calculator)), a {_a}, b{_b}, c{_c}, d{_d}, V_spike{_V_spike}, k1{_k1}, k2{_k2}, k3{_k3} {}
 
 void IzhikevichModelGPU::update_activity(RelearnGPUTypes::step_type step) {
     init_izhikevich_kernel(step, gpu_handle.get(), number_neurons, model_data_handle.get(), V_spike, a, b, c, d, k1, k2, k3, scale, h);
